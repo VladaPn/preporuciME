8942be40a64a6300213db918629568f8fd09695b
Vlada <vladapnpmf@gmail.com>
2024-08-14 22:23:31 +0200

    responsive hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <div className={`hero ${theme?'theme-dark':''}`}>
       <div className="left-hero">
         <div className={`box-rounded ${theme?'box-dark':''}`}></div>
-        <h2>Dobrodošli na PreporučiME</h2>
+        <h2>Dobrodošli <br className='novi' /> na <br className='novi' /> PreporučiME</h2>
         <p> platformu koja vas povezuje sa najboljim pružaocima usluga i omogućava vam da zaradite dok preporučujete druge! Bilo da nudite uslugu ili tražite pouzdane stručnjake, ovde je mesto gde vaša reputacija raste. Objavite svoj oglas, pružite vrhunske usluge, i dozvolite vašim zadovoljnim korisnicima da vas preporuče. Svaka preporuka donosi zaradu – kako vama, tako i onima koji vas preporučuju. <br /><span>Pridružite se zajednici gde se kvalitet prepoznaje i nagrađuje!</span></p>
         <button className='prijava'>Prijavi se</button> <button className='prijava'>Registruj se</button>
       </div>
